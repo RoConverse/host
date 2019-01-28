7958d116b70afd8e67fe92aeeb5f926f3d04c204
RoConverse <43471842+RoConverse@users.noreply.github.com>
2019-01-28 19:48:15 +0530

    Update host.py

--- host.py
+++ host.py
@@ -1,4 +1,7 @@
 from discord.ext import commands
 import os
 client=commands.Bot("sdadasjkdaksjnadk")
+@client.event
+async def on_ready():
+  await client.change_presence(game=discord.Game(name="with kermit"))
 client.run(os.getenv("token"), bot=False)
