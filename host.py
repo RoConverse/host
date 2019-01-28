e0ba7b7412e062bce9d5b9c5ac4cb5b0a37def43
RoConverse <43471842+RoConverse@users.noreply.github.com>
2019-01-28 19:28:20 +0530

    Update host.py

--- host.py
+++ host.py
@@ -1,15 +1,4 @@
-import discord
-from discord.ext.commands import Bot
 from discord.ext import commands
-import asyncio
-from discord import Game
 import os
-
-token = os.getenv("token")
-email = os.getenv("email")
-password = os.getenv("password")
-Client = discord.client
-client = commands.Bot(command_prefix = '3182309139')
-Clientdiscord = discord.Client()
-
-client.run(email, password)
+client=commands.Bot("sdadasjkdaksjnadk")
+client.run(os.getenv("token"), bot=False)
